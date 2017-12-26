f6ef03b26287668c0d58f222483375752bf434db
myyusuf <myyusuf1911@gmail.com>
2017-12-27 05:53:45 +0700

    Month year select

--- DPE Dashboard/Controllers/MainViewController.swift
+++ DPE Dashboard/Controllers/MainViewController.swift
@@ -13,11 +13,16 @@ class MainViewController: UIViewController, MonthYearPickerDelegate {
     var selectedMonth: Int = Constant.defaultMonth
     var selectedYear: Int = Constant.defaultYear
     let SHOW_MONTH_YEAR_PICKER_SEGUE = "ShowMonthYearPickerSegue"
-
+    
+    @IBOutlet weak var monthYearLabel: UIButton!
+    
+    let decimalFormatter = NumberFormatter()
+    let numberFormatter = NumberFormatter()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        initFormatter()
+        initMonthYear()
     }
 
     private func showFilter() {
@@ -28,11 +33,15 @@ class MainViewController: UIViewController, MonthYearPickerDelegate {
         showFilter()
     }
     
+    private func setMonthYearLabel() {
+        self.monthYearLabel.setTitle("\(Constant.months[self.selectedMonth - 1]), \(self.selectedYear)", for: .normal)
+    }
+    
     func monthYearSelected(month: Int, year: Int) {
-//        self.selectedMonth = month
-//        self.selectedYear = year
-//        setMonthYearLabel()
-//
+        self.selectedMonth = month
+        self.selectedYear = year
+        setMonthYearLabel()
+
 //        loadTableData()
     }
     
@@ -41,6 +50,33 @@ class MainViewController: UIViewController, MonthYearPickerDelegate {
         destinationVC?.selectedYear = self.selectedYear
         destinationVC?.selectedMonth = self.selectedMonth
         destinationVC?.delegate = self
+    }
+    
+    @IBAction func monthYearButtonDidSelect(_ sender: Any) {
+        showFilter()
+    }
+    
+    private func initMonthYear() {
+        let date = Date()
+        let calendar = Calendar.current
+        
+        let year = calendar.component(.year, from: date)
+        let month = calendar.component(.month, from: date)
+        
+        self.selectedMonth = month
+        self.selectedYear = year
+        
+        setMonthYearLabel()
+    }
+    
+    private func initFormatter() {
+        self.decimalFormatter.numberStyle = NumberFormatter.Style.decimal
+        self.decimalFormatter.minimumFractionDigits = 2
+        self.decimalFormatter.maximumFractionDigits = 2
+        
+        self.numberFormatter.numberStyle = NumberFormatter.Style.decimal
+        self.numberFormatter.minimumFractionDigits = 0
+        self.numberFormatter.maximumFractionDigits = 0
     }
     
 }
