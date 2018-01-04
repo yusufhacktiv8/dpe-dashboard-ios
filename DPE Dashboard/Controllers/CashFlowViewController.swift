6ff4d7177d5745101e40fdc6fa394463980ac7d6
myyusuf <myyusuf1911@gmail.com>
2018-01-05 00:36:29 +0700

    Cash flow

---
+++ DPE Dashboard/Controllers/CashFlowViewController.swift
@@ -0,0 +1,36 @@
+//
+//  CashFlowViewController.swift
+//  DPE Dashboard
+//
+//  Created by Muhammad Yusuf on 1/5/18.
+//  Copyright © 2018 Department Power Plant & Energy. All rights reserved.
+//
+
+import UIKit
+
+class CashFlowViewController: UIViewController {
+
+    @IBOutlet weak var monthYearLabel: UIButton!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
